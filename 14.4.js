1f1f8288383bfd9e790f080e7f3da36e41940e7e
Name <email@example.ru>
2022-10-23 12:34:38 +0500

    some fixes for JS 14.4

--- 14.4.js
+++ 14.4.js
@@ -1,26 +1,34 @@
 
 // Создаем promise
 const myPromise = new Promise((resolve, reject) => {
-let randNumber = Math.ceil(Math.random()*100);
-  
-if (randNumber %2 === 0) {
-    resolve({
-        message: "Завершено успешно.",
-        number: randNumber,
-      });
-  } else {
-    reject({
-        message: "Завершено с ошибкой.",
-        number: randNumber,
-      });
-  }
+    let randNumber = 0;
+    let rand = () => {
+        randNumber = Math.ceil(Math.random()*100)
+        if (randNumber %2 === 0) {
+            resolve({
+                message: "Завершено успешно.",
+                number: randNumber,
+            });
+        } else {
+            reject({
+                message: "Завершено с ошибкой.",
+                number: randNumber,
+            });
+        }
+    }
+    setTimeout(rand, 3000)
+    
 });
 
 // Выполняем promise
-myPromise
-  .then((result) => {
-    console.log(`${result.message} Сгенерированное число — ${result.number}`);
-  })
-  .catch((error) => {
-    console.log(`${error.message} Сгенерированное число — ${error.number}`);
-  });
+ const getMyPromise = async() => {
+    const promiseResult = await myPromise;
+    try {
+        console.log(`${await promiseResult.message} Сгенерированное число — ${await promiseResult.number}`);
+    }
+    catch(promiseResult) {
+        console.log(`${promiseResult.message} Сгенерированное число — ${promiseResult.number}`)
+    };
+}
+
+getMyPromise()
